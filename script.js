db794af52d5e96505cee49991dae48d73fdf3323
Thalison Campos <thalison1998@gmail.com>
2021-09-24 14:26:50 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const inputCreateDiv = document.querySelector('#inputDisparar')
+const containerDivs = document.querySelector('.container-divs')
+
+const create = () => {
+    const div = document.createElement('div')
+    div.classList.add('divs')
+    containerDivs.appendChild(div)
+}
+
+const removeDiv = () => {
+    const divs = containerDivs.querySelector(".divs")
+    containerDivs.removeChild(divs)
+}
+
+inputCreateDiv.addEventListener('change',()=>{
+    const valueInput = Number(inputCreateDiv.value)
+    for(let i = 0; i < valueInput;i++){
+        if(valueInput > 0){
+            create()
+        }
+    }
+   if(valueInput < 0){
+      const negativeTransformation  = Math.abs(valueInput)
+      for(let i = 0; i < negativeTransformation;i++){
+         removeDiv()
+      }
+   }
+
+})
